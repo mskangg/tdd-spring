8afe2a789ab44a4d85f572f4db81097e5a0f2814
mskangg <funnycoding91@gmail.com>
2021-03-24 23:20:26 +0900

    project init

---
+++ src/test/java/com/github/mskangg/tdd/spring/TddSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mskangg.tdd.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
